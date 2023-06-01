dc801abac18bb2c61c43154f1e2c9cc71785add4
Pablo <cv.juanp@gmail.com>
2023-06-01 09:56:22 -0700

    UPD env types

--- next.config.js
+++ next.config.js
@@ -7,5 +7,6 @@ module.exports = {
   env: {
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    MONGODB_URI: process.env.MONGODB_URI,
   },
 };
--- types/env.d.ts
+++ types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
+    MONGODB_URI: string;
   }
 }
