0cf787948455b4b0c729ba5e646e5c986f49a6a8
Pablo <cv.juanp@gmail.com>
2023-06-05 16:14:25 -0700

    ADD Post route for products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,14 +1,43 @@
 import { NextResponse } from "next/server";
-import { dbClient, dbConnect, dbClose } from "../../../lib/db";
+import { dbClient } from "../../../lib/db";
+import { validateNewProduct } from "@/utils/validateNewProduct";
+import { Product } from "@/types/product";
 
 //Return all products from the database
 export async function GET(request: Request) {
   try {
-    await dbConnect();
     const productCollection = dbClient.db("Cluster0").collection("products");
     const allProducts = await productCollection.find().toArray();
-    return NextResponse.json(allProducts);
-  } finally {
-    await dbClose();
+    return NextResponse.json(allProducts, { status: 200 });
+  } catch (error) {
+    console.log("error has occurred", error);
+  }
+}
+
+//Add new product to database
+export async function POST(request: Request) {
+  const res: Product = await request.json();
+  //Validate request
+  const errors = validateNewProduct(res);
+  //return error message if found
+  if (errors.length !== 0) {
+    return NextResponse.json(errors);
+  }
+  //if no errors save product in database
+  const newProduct: Product = {
+    name: res.name,
+    category: res.category,
+    img: res.img,
+    stock: res.stock,
+    price: res.price,
+    discountPercentage: res.discountPercentage,
+  };
+  try {
+    const productCollection = dbClient.db("Cluster0").collection("products");
+    await productCollection.insertOne(newProduct);
+    return NextResponse.json(newProduct, { status: 201 });
+  } catch (error) {
+    console.log("an error occurred", error);
+    return NextResponse.json({ status: 400 });
   }
 }
