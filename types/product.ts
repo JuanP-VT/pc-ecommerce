1a31d0c860a4714132cb4f3558d0e87e1f8f653a
Pablo <cv.juanp@gmail.com>
2023-06-06 14:43:04 -0700

    UPD types

--- types/product.ts
+++ types/product.ts
@@ -1,8 +1,18 @@
+import { User } from "./user";
+
 export type Product = {
   name: string;
   category: string;
+  description: string;
   img: string;
   stock: number;
   price: number;
   discountPercentage: number;
+  reviews: Review[];
+};
+
+type Review = {
+  stars: 1 | 2 | 3 | 4 | 5;
+  user: User;
+  comment: string;
 };
---
+++ types/user.ts
@@ -0,0 +1,11 @@
+import { Product } from "./product";
+
+export type User = {
+  _id: string;
+  name: string;
+  email: string;
+  rol: string;
+  image: string;
+  cash: number;
+  items: Product[];
+};
