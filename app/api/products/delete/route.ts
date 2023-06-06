40cd7a1c4d688ecd5c4727cb3eb7d9bf64754f53
Pablo <cv.juanp@gmail.com>
2023-06-05 17:41:51 -0700

    ADD Delete route for products

---
+++ app/api/products/delete/route.ts
@@ -0,0 +1,16 @@
+import { ObjectId, WithId } from "mongodb";
+import { NextResponse } from "next/server";
+import { dbClient } from "../../../../lib/db";
+// Delete product from the collection
+export async function POST(request: Request) {
+  try {
+    const res: WithId<Request> = await request.json();
+    const targetId = new ObjectId(res._id);
+    const collection = dbClient.db("Cluster0").collection("products");
+    await collection.findOneAndDelete({ _id: targetId });
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log("error has occurred", error);
+    return NextResponse.json({ status: 400 });
+  }
+}
