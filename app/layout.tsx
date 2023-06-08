4a326d47074982ee5335974a9a6230b936079647
Pablo <cv.juanp@gmail.com>
2023-06-08 15:52:19 -0700

    UPD layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
   const session = await getServerSession(OPTIONS);
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} relative `}>
         {" "}
         <Navigation session={session} />
         {children}
