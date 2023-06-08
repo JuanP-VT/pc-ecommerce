70c73bd74021d08e02149a3fc34844d40c6cd4f0
Pablo <cv.juanp@gmail.com>
2023-06-08 16:44:48 -0700

    UPD form style

--- components/forms/NewProductForm.tsx
+++ components/forms/NewProductForm.tsx
@@ -51,6 +51,17 @@ function NewProductForm({}: Props) {
             placeholder="Image"
           ></input>
         </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Price
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="number"
+            placeholder="Price"
+          ></input>
+        </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Current Stock
