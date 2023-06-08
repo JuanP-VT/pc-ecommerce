0eca70a9b59745c33c9734e8459cf7dec3053b0d
Pablo <cv.juanp@gmail.com>
2023-06-08 12:54:14 -0700

    Upd dependencies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {};
 module.exports = nextConfig;
 
 module.exports = {
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
   env: {
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
