437824049cb56f9e50ed45d0e5735278eab383e3
Pablo <cv.juanp@gmail.com>
2023-06-09 15:07:11 -0700

    ADD FormFeedback component + test suite

---
+++ __test__/forms/FormFeedback.test.tsx
@@ -0,0 +1,58 @@
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import FormFeedback from "@/components/forms/FormFeedback";
+import { Product } from "@/types/product";
+
+//Displays nothing if there are no errors
+it("should display nothing if there are no errors", () => {
+  const product: Product = {
+    name: "123", //
+    category: "Electronics",
+    price: 49.99,
+    description: [],
+    img: "https://example.com/product.jpg",
+    stock: 10,
+    discountPercentage: 20,
+  };
+  const rnd = render(<FormFeedback newProduct={product} />);
+  const list = screen.getByRole("list").querySelectorAll("li");
+  expect(list.length).toBe(0);
+});
+
+it("should display all errors", () => {
+  const product: any = {
+    name: "123",
+    category: "Electronics",
+    price: "49.99", //invalid must be string
+    description: [],
+    img: "https://example.com/product.jpg",
+    stock: "10", //invalid must be string
+    discountPercentage: -20,
+  };
+  const rnd = render(<FormFeedback newProduct={product} />);
+  const list = screen.getByRole("list").querySelectorAll("li");
+  expect(list.length).toBe(3);
+});
+
+it("should display the correct error logs", async () => {
+  const product: any = {
+    name: "123",
+    category: "Electronics",
+    price: -0.99,
+    description: [],
+    img: "https://example.com/product.jpg",
+    stock: -20, // cant be negative
+    discountPercentage: -20, // cant be negative
+  };
+  render(<FormFeedback newProduct={product} />);
+  await expect(
+    screen.findByText(/Stock cannot be negative/)
+  ).resolves.toBeInTheDocument();
+  await expect(
+    screen.findByText(/Discount cannot be negative/)
+  ).resolves.toBeInTheDocument();
+  await expect(
+    screen.findByText(/Price cannot be negative/)
+  ).resolves.toBeInTheDocument();
+});
---
+++ components/forms/FormFeedback.tsx
@@ -0,0 +1,27 @@
+import { Product } from "@/types/product";
+import validateNewProduct from "@/utils/validateProduct";
+import { error } from "console";
+import React from "react";
+
+type Props = {
+  newProduct: Product;
+};
+
+function FormFeedback({ newProduct }: Props) {
+  const errors = validateNewProduct(newProduct);
+  if (errors.length > 0) {
+  }
+  return (
+    <>
+      <ol>
+        {errors.map((err, index) => (
+          <li className="text-red-600" key={`i${index}`}>
+            {err}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
+
+export default FormFeedback;
