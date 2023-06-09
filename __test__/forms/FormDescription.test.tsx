7fc2c09fed553d62cf923e0667b58421bbfe7dd2
Pablo <cv.juanp@gmail.com>
2023-06-09 10:45:18 -0700

    ADD FormDescription component + test suite

---
+++ __test__/forms/FormDescription.test.tsx
@@ -0,0 +1,54 @@
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, fireEvent, getByText } from "@testing-library/react";
+import FormDescription from "@/components/forms/FormDescription";
+
+it("should render with one textarea at start ", async () => {
+  const setDescriptionMock = jest.fn();
+  const rnder = render(<FormDescription setDescription={setDescriptionMock} />);
+  const textAreas = (await screen.findByTestId("textAreas")).querySelectorAll(
+    "textarea"
+  );
+  expect(textAreas.length).toBe(1);
+});
+
+it("should render a new text area on 'Add' button click ", async () => {
+  const setDescriptionMock = jest.fn();
+  const rnder = render(<FormDescription setDescription={setDescriptionMock} />);
+  const addButton = rnder.getByText("Add");
+  fireEvent.click(addButton);
+  const textAreas = (await screen.findByTestId("textAreas")).querySelectorAll(
+    "textarea"
+  );
+  expect(textAreas.length).toBe(2);
+});
+
+it("should not delete the textarea if there is only one ", async () => {
+  const setDescriptionMock = jest.fn();
+  const rnder = render(<FormDescription setDescription={setDescriptionMock} />);
+  const remButton = rnder.getByText("Remove");
+  fireEvent.click(remButton);
+  fireEvent.click(remButton);
+  fireEvent.click(remButton);
+  fireEvent.click(remButton);
+  const textAreas = (await screen.findByTestId("textAreas")).querySelectorAll(
+    "textarea"
+  );
+  expect(textAreas.length).toBe(1);
+});
+
+//When the input changes, does it update the state correctly?
+it("should handle text area change and update textAreas and setDescription", async () => {
+  const setDescriptionMock = jest.fn();
+
+  const { getByLabelText } = render(
+    <FormDescription setDescription={setDescriptionMock} />
+  );
+  const textArea = (await screen.findByTestId("textAreas")).querySelectorAll(
+    "textarea"
+  )[0];
+
+  fireEvent.change(textArea, { target: { value: "New description" } });
+  expect(textArea.value).toBe("New description");
+  expect(setDescriptionMock).toHaveBeenCalledWith(["New description"]);
+});
---
+++ components/forms/FormDescription.tsx
@@ -0,0 +1,80 @@
+// This component dynamically creates text areas and saves its content value in an array of string
+// This array of string is going to be the property "descriptions" in a new product
+import { Dispatch, SetStateAction, useState } from "react";
+import handleTextAreaChange from "./handleTextAreaChange";
+type Props = {
+  setDescription: Dispatch<SetStateAction<string[]>>;
+};
+
+function FormDescription({ setDescription }: Props) {
+  const [numberOfInputs, setNumberOfInputs] = useState(1);
+  const [textAreas, setTextAreas] = useState<string[]>([]);
+
+  // For every "numberOfInputs" create a text area, each text area will save
+  // its content value in the "textAreas" State
+  // Each text area must have a key to make reference to its index in "textAreas"
+  const renderTextAreas = () => {
+    const items = [];
+    for (let index = 0; index < numberOfInputs; index++) {
+      items.push(
+        <textarea
+          onChange={(e) =>
+            handleTextAreaChange(
+              e,
+              textAreas,
+              setTextAreas,
+              index,
+              setDescription
+            )
+          }
+          key={index}
+          rows={2}
+          className="block p-2.5 w-full text-sm text-gray-900
+            bg-gray-50 rounded-lg border border-gray-300 
+            focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700
+            dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+            dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        ></textarea>
+      );
+    }
+    return items;
+  };
+  return (
+    <div className="mb-4" data-testid="textAreas">
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        Category
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold  px-4 border border-gray-400 rounded shadow"
+          onClick={(e) =>
+            setNumberOfInputs((state) => {
+              e.preventDefault();
+              return state + 1;
+            })
+          }
+        >
+          Add
+        </button>
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold  px-4 border border-gray-400 rounded shadow"
+          onClick={(e) => {
+            e.preventDefault();
+            if (numberOfInputs > 1) {
+              setNumberOfInputs((state) => state - 1);
+            }
+            const updTextAreas = [...textAreas];
+            updTextAreas.pop();
+            if (updTextAreas) {
+              setTextAreas(updTextAreas);
+              setDescription(updTextAreas);
+            }
+          }}
+        >
+          Remove
+        </button>
+        {renderTextAreas()}
+      </label>
+    </div>
+  );
+}
+
+export default FormDescription;
