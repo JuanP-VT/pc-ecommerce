1af61fec996b9e201d2af93c68629e5df7278b81
Pablo <cv.juanp@gmail.com>
2023-06-12 17:26:26 -0700

    UPD Product card component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import AdminProductCard from "@/components/product card/admin product card/AdminProductCard";
 
 type Props = {};
 
-function page({}: Props) {
-  return <div>page</div>;
+async function Page({}: Props) {
+  const res = await fetch("http://localhost:3000/api/products");
+  const data = await res.json();
+  return (
+    <div>
+      <AdminProductCard productList={data} />
+    </div>
+  );
 }
 
-export default page;
+export default Page;
--- components/product card/admin product card/AdminProductCard.tsx
+++ components/product card/admin product card/AdminProductCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import AdminProductCardEdit from "./AdminProductCardEdit";
 import AdminProductCardDisplay from "./AdminProductCardDisplay";
