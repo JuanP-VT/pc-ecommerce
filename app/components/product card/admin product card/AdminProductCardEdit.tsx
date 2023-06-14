456fc4c20a6b461c2000d29b9bca2e7bf1bbd8e1
Pablo <cv.juanp@gmail.com>
2023-06-14 15:26:05 -0700

    UPD admin product card

--- app/components/product card/admin product card/AdminProductCardEdit.tsx
+++ app/components/product card/admin product card/AdminProductCardEdit.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { ProductWithId } from "../../../types/product";
 import CardHeader from "../CardHeader";
 
@@ -7,7 +7,19 @@ type Props = {
   product: ProductWithId;
   setIsOnEditMode: Dispatch<SetStateAction<boolean>>;
 };
+//This component render a card with input fields related to the props of the object it represents
 function AdminProductCardEdit({ product, setIsOnEditMode }: Props) {
+  const [editedProduct, setEditedProduct] = useState<ProductWithId>({
+    _id: product._id,
+    name: product.name,
+    category: product.category,
+    description: product.description,
+    img: product.img,
+    stock: product.price,
+    price: product.price,
+    discountPercentage: product.discountPercentage,
+  });
+  console.log(editedProduct);
   return (
     <div className="w-80 flex flex-col border ">
       <CardHeader id={product._id} setIsOnEditMode={setIsOnEditMode} />
@@ -27,6 +39,9 @@ function AdminProductCardEdit({ product, setIsOnEditMode }: Props) {
             type="text"
             defaultValue={product.name}
             placeholder="Product Name"
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, name: e.target.value })
+            }
           />
         </div>
         <div className="mb-2 flex ">
@@ -42,6 +57,76 @@ function AdminProductCardEdit({ product, setIsOnEditMode }: Props) {
             type="text"
             defaultValue={product.category}
             placeholder="Product Category"
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, category: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="mb-2 flex ">
+          <label
+            className="text-gray-700 flex w-1/4 justify-start items-center text-sm font-bold "
+            htmlFor="stock"
+          >
+            Stock
+          </label>
+          <input
+            className="shadow appearance-none text-sm border rounded w-3/4 p-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="stock"
+            type="number"
+            defaultValue={product.stock}
+            placeholder="Current Stock"
+            onChange={(e) =>
+              setEditedProduct({
+                ...editedProduct,
+                stock: parseInt(e.target.value),
+              })
+            }
+          />
+        </div>
+
+        <div className="mb-2 flex ">
+          <label
+            className="text-gray-700 flex w-1/4 justify-start items-center text-sm font-bold "
+            htmlFor="price"
+          >
+            Price
+          </label>
+          <input
+            className="shadow appearance-none text-sm border rounded w-3/4 p-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="price"
+            type="number"
+            step={0.001}
+            defaultValue={product.price}
+            placeholder="Price"
+            onChange={(e) =>
+              setEditedProduct({
+                ...editedProduct,
+                price: parseInt(e.target.value),
+              })
+            }
+          />
+        </div>
+
+        <div className="mb-2 flex ">
+          <label
+            className="text-gray-700 flex w-1/4 justify-start items-center text-sm font-bold "
+            htmlFor="discount"
+          >
+            Discount
+          </label>
+          <input
+            className="shadow appearance-none text-sm border rounded w-3/4 p-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="discount"
+            type="number"
+            defaultValue={product.discountPercentage}
+            placeholder="Discount  %"
+            onChange={(e) =>
+              setEditedProduct({
+                ...editedProduct,
+                discountPercentage: parseInt(e.target.value),
+              })
+            }
           />
         </div>
       </form>
