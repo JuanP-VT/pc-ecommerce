5da2d2cdac3f9bc23456ad81e81c8fe82c7307ff
Pablo <cv.juanp@gmail.com>
2023-06-21 10:28:07 -0700

    ADD PriceTag Component

---
+++ app/components/product card/store product card/PriceTag.tsx
@@ -0,0 +1,29 @@
+/**
+ * Renders JSX conditionally if the price is whole integer or not
+ */
+import formatPrice from "@/app/utils/formatPrice";
+
+type Props = {
+  price: number;
+};
+
+function PriceTag({ price }: Props) {
+  const formattedPrice = formatPrice(price);
+  console.log(formattedPrice);
+  //price is fractional "eg 10.99"
+  if (formattedPrice.length === 2) {
+    return (
+      <div className="relative flex text-xl font-bold">
+        <span>{formattedPrice[0]}</span>
+        <span className="text-xs font-semibold ">{formattedPrice[1]}</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <span>{formattedPrice[0]}</span>
+    </div>
+  );
+}
+
+export default PriceTag;
