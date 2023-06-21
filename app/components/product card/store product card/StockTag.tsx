e762a965697e389e7f66466fa49c56e102ea0676
Pablo <cv.juanp@gmail.com>
2023-06-21 12:17:09 -0700

    ADD StockTag component

---
+++ app/components/product card/store product card/StockTag.tsx
@@ -0,0 +1,30 @@
+/**
+ * Renders JSX conditionally of Stock
+ * Currently there are three states:
+ * 1- No Stock
+ * 2- Low Stock -- arbitrary set to 22
+ * 3- Above Low Stock -- above 22
+ */
+type Props = {
+  stock: number;
+};
+
+function StockTag({ stock }: Props) {
+  if (stock === 0) {
+    return <p className="font-bold text-slate-600">Out Of Stock</p>;
+  }
+  if (stock < 22) {
+    return (
+      <p className="font-bold text-red-900">
+        Only {stock} units left in stock!
+      </p>
+    );
+  }
+  return (
+    <div className="flex">
+      <p className="font-bold text-green-500">Stock</p>
+    </div>
+  );
+}
+
+export default StockTag;
