682b0c01693b16458fc7ada0383aeff4c3af465f
Pablo <cv.juanp@gmail.com>
2023-06-20 20:42:40 -0700

    ADD  PageStore

---
+++ app/store/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex w-full h-full">
+        <div className="w-1/5  h-screen p-5">Sidebar</div>
+        <div className="w-4/5">Main</div>
+    </div>
+  )
+}
