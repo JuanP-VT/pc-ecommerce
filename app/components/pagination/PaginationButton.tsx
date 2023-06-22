1f47f80eaea6941c97063c9a84c2044da2952607
Pablo <cv.juanp@gmail.com>
2023-06-22 15:22:43 -0700

    ADD Pagination Button component

--- app/components/pagination/PaginationButton.tsx
+++ app/components/pagination/PaginationButton.tsx
@@ -1,6 +1,6 @@
 /**
  * Button for the pagination footer
- * Render JSX conditionally based on the current page
+ * Render JSX conditionally based on current page
  */
 import { Dispatch, SetStateAction } from "react";
 type Props = {
@@ -12,17 +12,20 @@ type Props = {
 function PaginationButton({ page, isCurrent, setCurrentPage }: Props) {
   if (isCurrent === true) {
     return (
-      <button
-        onClick={() => setCurrentPage(page)}
-        className="rounded-sm bg-sky-500 px-4 py-2  text-white hover:bg-sky-800"
-      >
+      <button className="rounded-sm bg-sky-500 px-4 py-2  text-white">
         {page}
       </button>
     );
   }
   return (
     <button
-      onClick={() => setCurrentPage(page)}
+      onClick={() => {
+        setCurrentPage(page);
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }}
       className="rounded-sm bg-sky-600 px-4 py-2  text-white hover:bg-sky-800"
     >
       {page}
---
+++ app/components/pagination/PaginationFillerDots.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+function PaginationFillerDots({}: Props) {
+  return (
+    <button className="rounded-sm bg-sky-600 px-4 py-2  text-white hover:bg-sky-800">
+      ....
+    </button>
+  );
+}
+
+export default PaginationFillerDots;
