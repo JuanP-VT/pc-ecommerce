2cf6ab305a349554a3a2dedaf7d02be6f7505a64
Pablo <cv.juanp@gmail.com>
2023-06-24 22:06:13 -0700

    UPD Fillerdots component

--- app/components/pagination/PaginationFillerDots.tsx
+++ app/components/pagination/PaginationFillerDots.tsx
@@ -2,7 +2,10 @@ type Props = {};
 
 function PaginationFillerDots({}: Props) {
   return (
-    <button className="rounded-sm bg-sky-600 px-4 py-2  text-white hover:bg-sky-800">
+    <button
+      data-testid="dots"
+      className="rounded-sm bg-sky-600 px-4 py-2  text-white hover:bg-sky-800"
+    >
       ....
     </button>
   );
