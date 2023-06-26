497f79d4dffa6ae91755e9838b6fe1d17f443bf2
Pablo <cv.juanp@gmail.com>
2023-06-26 12:15:25 -0700

    ADD dynamic routes for products

---
+++ app/[id]/page.tsx
@@ -0,0 +1,12 @@
+import ProductPage from "../components/product page/ProductPage";
+import searchProductInDb from "../lib/searchProductInDb";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  //Check if product exist
+  const product = await searchProductInDb(params.id);
+
+  if (product) {
+    return <ProductPage product={product} />;
+  }
+  return <div>product not found</div>;
+}
