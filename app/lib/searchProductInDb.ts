7acc5d0e393df0471dc14dca5684bfee2d66e1f8
Pablo <cv.juanp@gmail.com>
2023-06-26 11:56:04 -0700

    ADD search product function

---
+++ app/lib/searchProductInDb.ts
@@ -0,0 +1,17 @@
+import { ObjectId } from "mongodb";
+import { dbClient } from "./db";
+import { ProductWithId } from "../types/product";
+export default async function searchProductInDb(
+  id: string
+): Promise<ProductWithId | null> {
+  try {
+    const prodId = new ObjectId(id);
+    const collection = dbClient.db("Cluster0").collection("products");
+    const find = (await collection.findOne({
+      _id: prodId,
+    })) as ProductWithId | null;
+    return find;
+  } catch (err) {
+    return null;
+  }
+}
