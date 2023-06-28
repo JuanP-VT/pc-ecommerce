4951b57332e2bfec5510daa47705056e3b5f533c
Pablo <cv.juanp@gmail.com>
2023-06-28 10:40:54 -0700

    Bugfix multiple pages not opening simultaneously

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,12 +1,24 @@
 import ProductPage from "../components/product page/ProductPage";
 import searchProductInDb from "../lib/searchProductInDb";
+import { ProductWithId } from "../types/product";
 
 export default async function Page({ params }: { params: { id: string } }) {
   //Check if product exist
+  const id = params.id;
   const product = await searchProductInDb(params.id);
 
   if (product) {
     return <ProductPage product={product} />;
   }
-  return <div>page not found</div>;
+  return <div>Product Not found</div>;
+}
+
+export async function generateStaticParams() {
+  const productList: ProductWithId[] = await fetch(
+    "https://store-juanp-vt.vercel.app/api/products"
+  ).then((res) => res.json());
+
+  return productList.map((prod) => {
+    prod._id;
+  });
 }
