1981fb16ba79acf39008b80126bd2dcbce55f7fb
Pablo <cv.juanp@gmail.com>
2023-06-27 18:36:36 -0700

    UPD Style

--- app/components/product page/About.tsx
+++ app/components/product page/About.tsx
@@ -6,7 +6,8 @@ type Props = {
 
 function About({ description }: Props) {
   return (
-    <li className="font-sm mb-3 rounded-md bg-gray-200 text-justify font-sans text-sm">
+    <li className="font-sm mb-3 flex items-center rounded-md bg-gray-200 p-2 text-justify font-sans text-sm">
+      <span className="marker mr-2 text-2xl">•</span>
       {description}
     </li>
   );
