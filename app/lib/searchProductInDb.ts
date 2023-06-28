128640382cedc839606a7e66b8e5937706e7aab1
Pablo <cv.juanp@gmail.com>
2023-06-28 13:21:28 -0700

    UPD product page rem static params

--- app/lib/searchProductInDb.ts
+++ app/lib/searchProductInDb.ts
@@ -5,13 +5,14 @@ export default async function searchProductInDb(
   id: string
 ): Promise<ProductWithId | null> {
   const res = await fetch("http://localhost:3000/api/searchproduct", {
+    cache: "no-store",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ _id: id }),
   });
-  const productList = res.json();
+  const productList = await res.json();
   if (productList) {
     return productList;
   }
