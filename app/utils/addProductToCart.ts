861fdbbb55b5d16c2dd8aa574229ad1977a89713
Pablo <cv.juanp@gmail.com>
2023-07-01 11:59:04 -0700

    ADD addProductToCart util function + test suite

---
+++ app/utils/addProductToCart.ts
@@ -0,0 +1,29 @@
+/**
+ * Adds or replaces a purchase order in the session cart array
+ * Returns the updated cart
+ */
+import { PurchaseOrder } from "../types/order";
+import getCartItems from "./getCartItems";
+export default function addProductToCart(
+  cartKey: string,
+  order: PurchaseOrder
+) {
+  const orderList = getCartItems(cartKey);
+  // Check if item is already in the cart
+  // If order already exist in cart, replace it with new one
+  const find = orderList.find((ord) => ord.product._id === order.product._id);
+  if (find) {
+    const index = orderList.indexOf(find);
+    orderList[index] = order;
+    sessionStorage.setItem(cartKey, JSON.stringify(orderList));
+    //return updated cart
+    const updatedCart = getCartItems(cartKey);
+    return updatedCart;
+  }
+  // add order if is not in the cart,
+  orderList.push(order);
+  sessionStorage.setItem(cartKey, JSON.stringify(orderList));
+  const updatedCart = getCartItems(cartKey);
+  return updatedCart;
+  //return updated cart
+}
