5cbea10271b015a6a9dbfb8922979ed9a8300554
Pablo <cv.juanp@gmail.com>
2023-07-05 11:45:57 -0700

    Add Cart Page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,32 +1,10 @@
-"use client";
-
 export const dynamic = "force-dynamic";
-
-import { useEffect, useState } from "react";
-import getCartItems from "../utils/getCartItems";
-import { PurchaseOrder } from "../types/order";
-import ShoppingCartCard from "../components/shopping cart/ShoppingCartCard";
-function Page() {
-  const [cartList, setCartList] = useState<PurchaseOrder[]>([]);
-  //Get cart list from session storage
-  useEffect(() => {
-    const key = process.env.SESSION_CART_KEY;
-    const list = getCartItems(key);
-    setCartList(list);
-  }, []);
-  return (
-    <div className="flex flex-col px-5 lg:px-14">
-      <h1 className="px-5 py-3 text-3xl font-semibold">Shopping Cart</h1>
-      <div className="flex flex-col p-5">
-        {cartList.map((order, index) => (
-          <ShoppingCartCard order={order} key={`prdcrt${index}`} />
-        ))}
-      </div>
-      <div className="flex">
-        <button className="flex">Pay</button>
-      </div>
-    </div>
-  );
+import CartPage from "./CartPage";
+import { OPTIONS } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+export default async function Page() {
+  const session = await getServerSession(OPTIONS);
+  if (session) {
+    return <CartPage session={session} />;
+  }
 }
-
-export default Page;
