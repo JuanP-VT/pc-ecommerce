0e3151577e48ee6115cef20b7350b59de6914710
Pablo <cv.juanp@gmail.com>
2023-07-09 21:06:45 -0700

    Upd server component fetch url

--- app/lib/searchProductInDb.ts
+++ app/lib/searchProductInDb.ts
@@ -1,17 +1,18 @@
-import { ObjectId } from "mongodb";
-import { dbClient, dbClose, dbConnect } from "./db";
 import { ProductWithId } from "../types/product";
 export default async function searchProductInDb(
   id: string
 ): Promise<ProductWithId | null> {
-  const res = await fetch("http://localhost:3000/api/searchproduct", {
-    cache: "no-store",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ _id: id }),
-  });
+  const res = await fetch(
+    "https://store-juanp-vt.vercel.app/store/api/searchproduct",
+    {
+      cache: "no-store",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ _id: id }),
+    }
+  );
   const productList = await res.json();
   if (productList) {
     return productList;
