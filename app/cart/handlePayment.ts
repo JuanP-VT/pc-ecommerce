021ffe213c30cdb74fcd85368b34c1414abc5741
Pablo <cv.juanp@gmail.com>
2023-07-23 12:39:43 -0700

    ADD / UPD - Documentation cart

--- app/cart/handlePayment.ts
+++ app/cart/handlePayment.ts
@@ -1,11 +1,22 @@
 /**
- * @param cart a list of purchase orders,
- * @param userID unique MongoDB ID for the user
- * @returns feedback as  JSON response from the API
+ * Handle Payment Function
  *
- * Sends the purchase order to the backend
- * Displays feedback in the DOM using  the document API, query selectors, i feel is an okay move
- * with react since it is going to be a very simple Dom manipulation.
+ * @param {PurchaseOrder[]} cart - A list of purchase orders representing the items in the cart.
+ * @param {string} userID - The unique MongoDB ID for the user making the payment.
+ * @param {Dispatch<SetStateAction<boolean>>} setIsLoading - A state setter function to control the loading state during payment processing.
+ * @param {AppRouterInstance} router - The Next.js app router instance.
+ *
+ * @returns {Promise<void>} - A promise that resolves after the payment processing is completed.
+ *
+ * This function sends the purchase order to the backend API for payment processing. It displays feedback in the DOM using the document API,
+ * query selectors, and simple DOM manipulation. The function first sets the loading state to true using setIsLoading to indicate that the payment
+ * is in progress. It then creates the authentication header using the userID with the createAuthHeader utility function. Next, it makes a POST
+ * request to the "/api/payment/" endpoint with the cart data and the authentication header. After receiving the response from the backend, it parses
+ * the JSON feedback and sets the loading state back to false.
+ *
+ * If the feedback indicates that the user does not have enough cash to make the payment, it displays a message "Not Enough Cash" in the DOM for a brief moment.
+ * If the payment is successful ("Success" feedback), it clears the sessionStorage, triggers a "storageUpdate" event, refreshes the router to update the UI with the new cart,
+ * and navigates the user back to the home page ("/").
  */
 import { Dispatch, SetStateAction } from "react";
 import { PurchaseOrder } from "../types/order";
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,10 @@
+/**
+ * Server Component
+ *
+ * It serves as a wrapper that checks the user's session using the `getServerSession` function from `next-auth`.
+ * If the user is authenticated (i.e., the session is available), it renders the `CartPage` component passing the session object as a prop. Otherwise, it renders the `NotFound`
+ * component with the message "You are not signed in". The `OPTIONS` object is used as a parameter for the `getServerSession` function to configure the session retrieval.
+ */
 export const dynamic = "force-dynamic";
 import CartPage from "./CartPage";
 import { OPTIONS } from "../api/auth/[...nextauth]/route";
